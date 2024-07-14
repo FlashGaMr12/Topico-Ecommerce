5f0de742696ce47b28ba3e36299198894e3ffdc5
Ahmad Hossam <51684620+FlashGaMr12@users.noreply.github.com>
2024-07-15 00:31:00 +0300

    re added loop

--- Js/swiper.js
+++ Js/swiper.js
@@ -9,6 +9,7 @@ var swiper = new Swiper(".slide-swp", {  //my class is slide-swp
     autoplay: {
       Delay: 2000,
     },
+    loop:true,
   });
 //  Swiper slide - sale Slide 
   var swiper = new Swiper(".sale_sec", {  //my class is slide-swp
@@ -17,11 +18,12 @@ var swiper = new Swiper(".slide-swp", {  //my class is slide-swp
       dynamicBullets: true,
       clickable: true,
     },
-    slidesPerView:2,
+    slidesPerView:5,
     spaceBetween:30,
     autoplay: {
       Delay: 3000,
     },
+         loop:true,
     navigation:{
         nextEl:".swiper-button-next",
         prevEl:".swiper-button-prev",
